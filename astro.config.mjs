b85a2a2b949c783ee9cc6e61bde18766ce5a5fe0
jicking <7297029+jicking@users.noreply.github.com>
2024-06-24 05:38:35 +0800

    set config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  site: "https://jicking.github.io",
+  integrations: [svelte()],
 });
